bf2b91a2db5435d8b0f2e65e57868ad08d2ad907
Albin Byström (Byssis) <albinbys@gmail.com>
2015-01-19 09:58:02 +0100

    Add validation for email

--- welcome.php
+++ welcome.php
@@ -2,7 +2,16 @@
 <body>
 
 Welcome <?php echo $_POST["fullName"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your email address is: <?php 
+$email = $_POST["email"];
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	echo "Invalid email format"; 
+}
+else {
+	echo $email; 
+}
+
+?>
 
 </body>
 </html>
